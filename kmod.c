075b4afe974fe06c338f72c4fcf1397352623cbb
KerneyJ <gamerguy9801@gmail.com>
2023-05-25 22:23:27 +0000

    probing handle_mm_fault too

--- kmod.c
+++ kmod.c
@@ -26,8 +26,10 @@ static struct sock *socket = NULL;
 
 static char sym_mmap[KSYM_NAME_LEN] = "do_mmap";
 static char sym_swap[KSYM_NAME_LEN] = "do_swap_page";
+static char sym_hmf[KSYM_NAME_LEN] = "handle_mm_fault"; // hmf = handle_mm_fault
 module_param_string(sym_mmap, sym_mmap, KSYM_NAME_LEN, 0664);
 module_param_string(sym_swap, sym_swap, KSYM_NAME_LEN, 0664);
+module_param_string(sym_hmf, sym_hmf, KSYM_NAME_LEN, 0664);
 
 static struct kprobe kp_mmap = {
 	.symbol_name = sym_mmap,
@@ -37,6 +39,9 @@ static struct kprobe kp_swap = {
 	.symbol_name = sym_swap,
 };
 
+static struct kprobe kp_hmf = {
+	.symbol_name = sym_hmf,
+};
 
 static int __kprobes kpmmap_pre(struct kprobe *p, struct pt_regs *regs){
 	//pr_info("<%s> p->addr = 0x%p, ip = %lx, flags = 0x%lx, pid = %d\n",
@@ -110,6 +115,43 @@ static void __kprobes kpswap_post(struct kprobe *p, struct pt_regs *regs, unsign
 		printk(KERN_INFO "Error while sending back to user\n");
 }
 
+static int __kprobes kphmf_pre(struct kprobe *p, struct pt_regs *regs){
+	//pr_info("<%s> p->addr = 0x%p, ip = %lx, flags = 0x%lx, pid = %d\n",
+	//		p->symbol_name, p->addr, regs->ip, regs->flags, current->pid);
+	return 0;
+}
+
+static void __kprobes kphmf_post(struct kprobe *p, struct pt_regs *regs, unsigned long flags){
+	struct nlmsghdr *nlh;
+	struct sk_buff *skb_out;
+	char msg[MAX_PAYLOAD];
+	int res, msg_size;
+
+	pr_info("symbol: %s, pid: %d\n", p->symbol_name, current->pid);
+	if(current->pid != stalkpid)
+		return;
+
+	if(pid < 0)
+		return;
+
+	snprintf(msg, MAX_PAYLOAD, "<%s>, p->addr = ox%p, flags = 0x%lx, pid = %d\n", p->symbol_name, p->addr, regs->flags, current->pid);
+	msg_size = strlen(msg);
+
+	skb_out = nlmsg_new(msg_size, 0);
+	if(!skb_out){
+		printk(KERN_ERR "Failed to allocate new skb\n");
+		return;
+	}
+
+	nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);
+	NETLINK_CB(skb_out).dst_group = 0;
+	strncpy(nlmsg_data(nlh), msg, msg_size);
+	res = nlmsg_unicast(socket, skb_out, pid);
+	if(res < 0)
+		printk(KERN_INFO "Error while sending back to user\n");
+}
+
+
 static void register_process(struct sk_buff *skb){
 	struct nlmsghdr *nlh;
 	struct sk_buff *skb_out;
@@ -156,6 +198,8 @@ static int __init kprobe_init(void){
 	kp_mmap.post_handler = kpmmap_post;
 	kp_swap.pre_handler = kpswap_pre;
 	kp_swap.post_handler = kpswap_post;
+	kp_hmf.pre_handler = kphmf_pre;
+	kp_hmf.post_handler = kphmf_post;
 	if( (ret = register_kprobe(&kp_mmap)) < 0){
 		pr_err("register kp_mmap failed, returned %d\n", ret);
 		return ret;
@@ -164,8 +208,13 @@ static int __init kprobe_init(void){
 		pr_err("register kp_wrpage failed, returned %d\n", ret);
 		return ret;
 	}
+	if( (ret = register_kprobe(&kp_hmf)) < 0){
+		pr_err("register kp_hmf failed, returned %d\n", ret);
+		return ret;
+	}
 	pr_info("Planted kprobe mmap at %p\n", kp_mmap.addr);
-	pr_info("Planted kprobe wrpage at %p\n", kp_swap.addr);
+	pr_info("Planted kprobe swap at %p\n", kp_swap.addr);
+	pr_info("Planted kprobe handle_mm_swap at %p\n", kp_hmf.addr);
 	return 0;
 }
 
@@ -174,6 +223,7 @@ static void __exit kprobe_exit(void){
 	unregister_kprobe(&kp_swap);
 	pr_info("kprobe mmap  %p unregistered\n", kp_mmap.addr);
 	pr_info("kprobe wrpage %p unregistered\n", kp_swap.addr);
+	pr_info("kprobe handle_mm_swap %p unregistered\n", kp_hmf.addr);
 
 	netlink_kernel_release(socket);
 }
